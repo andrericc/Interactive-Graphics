6488bdd59e51134a7a82ea40aac615720d59c2cb
andrericc <61393647+andrericc@users.noreply.github.com>
2025-04-10 12:09:30 +0200

    Update project1.js
    Homework solved

--- HW1-ComposingImages/project1.js
+++ HW1-ComposingImages/project1.js
@@ -2,6 +2,33 @@
 // fgImg is the foreground image.
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
-function composite( bgImg, fgImg, fgOpac, fgPos )
-{
+function composite(bgImg, fgImg, fgOpac, fgPos) {
+    const bgPixels = bgImg.data;
+    const fgPixels = fgImg.data;
+    const bgW = bgImg.width;
+    const bgH = bgImg.height;
+    const fgW = fgImg.width;
+    const fgH = fgImg.height;
+
+    for (let j = 0; j < fgH; j++) {
+        for (let i = 0; i < fgW; i++) {
+            const destX = i + fgPos.x;
+            const destY = j + fgPos.y;
+
+            if (destX < 0 || destY < 0 || destX >= bgW || destY >= bgH) {
+                continue;
+            }
+
+            const fgIdx = (j * fgW + i) * 4;
+            const bgIdx = (destY * bgW + destX) * 4;
+
+            const alphaFg = (fgPixels[fgIdx + 3] / 255) * fgOpac;
+            const alphaBg = 1 - alphaFg;
+
+            bgPixels[bgIdx]     = fgPixels[fgIdx]     * alphaFg + bgPixels[bgIdx]     * alphaBg;
+            bgPixels[bgIdx + 1] = fgPixels[fgIdx + 1] * alphaFg + bgPixels[bgIdx + 1] * alphaBg;
+            bgPixels[bgIdx + 2] = fgPixels[fgIdx + 2] * alphaFg + bgPixels[bgIdx + 2] * alphaBg;
+            bgPixels[bgIdx + 3] = 255;
+        }
+    }
 }
